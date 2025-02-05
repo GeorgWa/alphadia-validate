62af080e2bb33f1dd1f540f9222bf60304a83523
caetera <homer2k@gmail.com>
2025-02-05 15:01:11 +0100

    Fragment matching tolerance implemented

--- notebooks/showcase/isoform_attention.py
+++ notebooks/showcase/isoform_attention.py
@@ -6,7 +6,7 @@ from alphabase.protein.fasta import SpecLibFasta
 from alphabase.spectral_library.flat import SpecLibFlat
 from .slicer import SpectrumSlicer, get_flat_library_entry_by_hash
 
-def fragment_isoform_annotation(precursor_entry, fixed_mods = {'Carbamidomethyl@C'}):    
+def fragment_isoform_annotation(precursor_entry, fixed_mods = {'Carbamidomethyl@C'}):  
     #parsing precursor entry
     sequence = precursor_entry['sequence']
     charge = precursor_entry['charge']
@@ -16,6 +16,7 @@ def fragment_isoform_annotation(precursor_entry, fixed_mods = {'Carbamidomethyl@
     else:
         mod_counter = Counter(precursor_entry['mods'].split(';'))
     
+    #count the number of variable modifications
     var_mod_count = [mod_counter[mod] for mod in mod_counter.keys() if mod not in fixed_mods]
     if len(var_mod_count) == 0:
         max_var_mod_num = 0
@@ -39,25 +40,27 @@ def fragment_isoform_annotation(precursor_entry, fixed_mods = {'Carbamidomethyl@
         max_var_mod_num=max_var_mod_num
     )
 
+    #Calculate fragment mzs
     fastalib.precursor_df = precursor_df
     fastalib.add_modifications()
     fastalib.calc_fragment_mz_df()
     flatlib = SpecLibFlat()
     flatlib.parse_base_library(fastalib)
 
-    #TODO look for better way to annotate isoforms
-    fragment_tables = []
+    #Annotate with isoforms
+    fragment_table = flatlib.fragment_df.copy()
+    fragment_table['isoform'] = 'None'
     for _, e in flatlib.precursor_df.iterrows():
-        t = flatlib.fragment_df[e['flat_frag_start_idx']:e['flat_frag_stop_idx']]
-        t['isoform'] = e['mod_sites']
-        fragment_tables.append(t)
+        if e['mod_sites'] != '':
+            fragment_table.loc[e['flat_frag_start_idx']:e['flat_frag_stop_idx'], 'isoform'] = e['mod_sites']
+ 
+    return fragment_table
 
-    #TODO better grouping?
-    isoform_map = pd.concat(fragment_tables).groupby('mz').agg({'isoform': lambda s: '+'.join(s.unique())})
+def find_isoform(mz, isoform_map, match_tolerance):
+    isoforms_ids = isoform_map.loc[np.abs(isoform_map['mz'] - mz) / mz < match_tolerance, 'isoform'].unique()
+    return '+'.join(isoforms_ids)
 
-    return isoform_map
-
-def isoform_attention_plot(spectral_library_flat, precursor_df, dia_data, selected_hash):
+def isoform_attention_plot(spectral_library_flat, precursor_df, dia_data, selected_hash, match_tolerance=7e-6):
 
     slicer = SpectrumSlicer(spectral_library_flat, precursor_df, dia_data)
     
@@ -66,10 +69,10 @@ def isoform_attention_plot(spectral_library_flat, precursor_df, dia_data, select
     mz_library, intensity_library, data_slice = slicer.get_by_hash(selected_hash)
 
     isoform_map = fragment_isoform_annotation(precursor_entry)
-    
+        
     library_spec = pd.DataFrame({'mz': mz_library, 'intensity': intensity_library})
-    library_spec['isoform'] = list(isoform_map.reindex(library_spec['mz'])['isoform'])
-
+    library_spec['isoform'] = library_spec['mz'].apply(find_isoform, args=(isoform_map, match_tolerance))
+    
     intensity_observed = data_slice[0].sum(axis=(1,2,3))
     intensity_observed_normalized = intensity_observed / intensity_observed.max()
     library_spec['intensity'] = library_spec['intensity'] / library_spec['intensity'].max()
