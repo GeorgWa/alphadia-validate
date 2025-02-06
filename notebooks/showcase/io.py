79007d93d92bf501f7f9cb76e9a7c805607df292
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-02-06 19:30:06 +0100

    allow user to provide files

--- notebooks/showcase/io.py
+++ notebooks/showcase/io.py
@@ -11,9 +11,8 @@ from alphadia.test_data_downloader import DataShareDownloader
 from alphabase.spectral_library.flat import SpecLibFlat
 
 # Bulk injections of HeLa cell lysate acquired on the Orbitrap Astral
-RAW_DATA_URL_LIST = (
-    "https://datashare.biochem.mpg.de/s/339jg5HtGrwLwDN/download?files=20231017_OA2_TiHe_ADIAMA_HeLa_200ng_Evo011_21min_F-40_07.raw",
-)
+RAW_DATA_URL =  "https://datashare.biochem.mpg.de/s/339jg5HtGrwLwDN/download?files=20231017_OA2_TiHe_ADIAMA_HeLa_200ng_Evo011_21min_F-40_07.raw"
+
 
 # results from search_1.10.0.ipynb
 PRECURSORS_TSV_URL = "https://datashare.biochem.mpg.de/s/fsCqlT757ttVWI8"
@@ -28,36 +27,53 @@ memory = Memory(".memoize_cachedir")
 @memory.cache
 def prepare_data(
     main_folder: Path,
-    save_pickle: bool = False
+    *,
+    download_data: bool = False,
+    save_pickle: bool = False,
+    raw_file_name: str = None,
+    precursors_file_name: str = None,
+    speclib_file_name: str = None,
 ) -> tuple[pd.DataFrame, SpecLibBase, SpecLibFlat, Thermo]:
-    """Prepare raw & results data.
+    """Prepare raw & results data and return data objects.
 
-    If the main_folder/output does not already contain results (precursors.tsv and speclib.hdf) from
-    a previous search, this function will download the raw data and return the dataframes and objects
-
-     Note: this is just required if you did not run the search yourself in search_1.10.0.ipynb
+    If the download_data is true, this function will download the required data.
 
     :param main_folder: folder in which data is expected / will be downloaded to
+    :param download_data: whether to download the data
+    :param save_pickle: whether to save the raw data to a pickle file (just for maintainers)
+    :param precursors_file_name: name of the precursors file (required if download_data is False)
+    :param raw_file_name: name of the raw file (required if download_data is False)
+    :param speclib_file_name: name of the speclib file (required if download_data is False)
     :return:
     """
-    precursor_df, current_raw_path, speclib_path = _download_data(main_folder)
+    if download_data:
+        precursors_tsv_path, raw_file_path, speclib_path = _download_data(main_folder)
+    else:
+        if precursors_file_name is None or raw_file_name is None or speclib_file_name is None:
+            raise ValueError("Please provide the file names for the precursors, raw file and speclib files")
+
+        precursors_tsv_path = main_folder / precursors_file_name
+        raw_file_path = main_folder / raw_file_name
+        speclib_path = main_folder / speclib_file_name
+
+    current_raw_name = os.path.basename(raw_file_path).replace(".raw", "")
+    precursor_df = pd.read_csv(precursors_tsv_path, sep="\t")
+    precursor_df = precursor_df[precursor_df["run"] == current_raw_name]
 
     spectral_library = SpecLibBase()
     spectral_library.load_hdf(speclib_path)
 
     # caching to pkl
-    pkl_path = Path(main_folder) / "output" / f"{Path(current_raw_path).stem}.pkl"
-    if pkl_path.exists():
+    if (pkl_path:= Path(main_folder) / f"{Path(raw_file_path).stem}.pkl").exists():
         print("loading raw data from pkl...")
         with open(pkl_path, 'rb') as file:
             dia_data = pickle.load(file)
     else:
-        dia_data = Thermo(current_raw_path)
+        dia_data = Thermo(raw_file_path)
         if save_pickle:
             print("saving raw data to pkl...")
             with open(pkl_path, 'wb') as file:
                 pickle.dump(dia_data, file)
-
 
     spectral_library_flat = SpecLibFlat()
     spectral_library_flat.parse_base_library(spectral_library)
@@ -65,28 +81,18 @@ def prepare_data(
     return precursor_df, spectral_library, spectral_library_flat, dia_data
 
 
-def _download_data(main_folder: Path) -> tuple[pd.DataFrame, str, str]:
+def _download_data(main_folder: Path) -> tuple[str, str, str]:
     """Download data if not already present."""
-    data_folder = main_folder / "data"
-    output_folder = main_folder / "output"
-    for folder in [data_folder, output_folder]:
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-    raw_file_paths = [
-        DataShareDownloader(url, str(data_folder)).download()
-        for url in RAW_DATA_URL_LIST
-    ]
+    os.makedirs(main_folder, exist_ok=True)
+
+    raw_file_path = DataShareDownloader(RAW_DATA_URL, str(main_folder)).download()
+
     precursors_tsv_path = DataShareDownloader(
-        PRECURSORS_TSV_URL, str(output_folder)
+        PRECURSORS_TSV_URL, str(main_folder)
     ).download()
-    speclib_path = DataShareDownloader(SPECLIB_URL, str(output_folder)).download()
+    speclib_path = DataShareDownloader(SPECLIB_URL, str(main_folder)).download()
 
-    current_raw_path = raw_file_paths[0]
-    current_raw_name = os.path.basename(current_raw_path).replace(".raw", "")
-    precursor_df = pd.read_csv(precursors_tsv_path, sep="\t")
-    precursor_df = precursor_df[precursor_df["run"] == current_raw_name]
-
-    return precursor_df, current_raw_path, speclib_path
+    return precursors_tsv_path, raw_file_path, speclib_path
 
 
 def display_spectral_library(spectral_library):
