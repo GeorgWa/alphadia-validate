21359b5b34a8153209bcda52bd8b19cd7041f1cf
caetera <homer2k@gmail.com>
2025-02-06 11:52:48 +0100

    Isoform ids from modifications

--- notebooks/showcase/isoform_attention.py
+++ notebooks/showcase/isoform_attention.py
@@ -7,6 +7,56 @@ from alphabase.spectral_library.flat import SpecLibFlat
 from alphabase.spectral_library.translate import create_modified_sequence
 from .slicer import SpectrumSlicer, get_flat_library_entry_by_hash
 
+def get_isoform_id(
+    mods,
+    mod_sites,
+    translate_mod_dict: dict = None,
+    ignore_mods={}
+):
+    """
+    Convert a list of modifications and their sites into a string representation of the isoform.
+    
+    Parameters
+    ----------
+    mods : str
+        A string of modifications separated by ';'.
+        e.g. 'Carbamidomethyl@C;Oxidation@M'
+    mod_sites : str
+        A string of modification sites separated by ';'.
+        e.g. '7;24'
+    
+    translate_mod_dict : dict
+        A dict to map AlphaX modification names to other software,
+        use unimod name if None.
+        Defaults to None.
+
+    ignore_mods : set
+        A set of modifications to ignore for isoform generation.
+        Defaults to {}.
+    """
+    if mods:
+        var_mods = []
+        var_mod_sites = []
+        for mod, site in zip(mods.split(";"), mod_sites.split(";")):
+            if mod not in ignore_mods:
+                var_mods.append(mod)
+                var_mod_sites.append(int(site))
+        order = np.argsort(var_mod_sites)
+        var_mod_sites = [var_mod_sites[order[i]] for i in range(len(var_mod_sites))]
+        var_mods = [var_mods[order[i]] for i in range(len(var_mods))]
+
+        if translate_mod_dict is None:
+            var_mods = [mod[: mod.find("@")] for mod in var_mods]
+        else:
+            var_mods = [translate_mod_dict[mod] for mod in var_mods]
+
+        isoform_id = ';'.join([f'{mod}{site}' for site, mod in zip(var_mod_sites, var_mods)])
+
+        if isoform_id == '':
+            isoform_id = 'Base'
+        
+        return isoform_id
+
 def fragment_isoform_annotation(precursor_entry, fixed_mods = {'Carbamidomethyl@C'}):  
     #parsing precursor entry
     sequence = precursor_entry['sequence']
@@ -50,10 +100,11 @@ def fragment_isoform_annotation(precursor_entry, fixed_mods = {'Carbamidomethyl@
 
     #Annotate with isoforms
     fragment_table = flatlib.fragment_df.copy()
-    fragment_table['isoform'] = 'None'
+    fragment_table['isoform'] = 'Base'
     for _, e in flatlib.precursor_df.iterrows():
         if e['mod_sites'] != '':
-            fragment_table.loc[e['flat_frag_start_idx']:e['flat_frag_stop_idx'], 'isoform'] = e['mod_sites']
+            fragment_table.loc[e['flat_frag_start_idx']:e['flat_frag_stop_idx'], 'isoform'] =\
+            get_isoform_id(e['mods'], e['mod_sites'], ignore_mods=fixed_mods)
  
     return fragment_table
 
@@ -78,12 +129,12 @@ def isoform_attention_plot(spectral_library_flat, precursor_df, dia_data, select
     intensity_observed_normalized = intensity_observed / intensity_observed.max()
     library_spec['intensity'] = library_spec['intensity'] / library_spec['intensity'].max()
 
-    plt.stem(mz_library, intensity_observed_normalized, markerfmt='None', basefmt='grey')
+    plt.stem(mz_library, intensity_observed_normalized, markerfmt='None', basefmt='grey', label='Observed')
     for n, (isoform_id, peak_group) in enumerate(library_spec.groupby('isoform')):
         plt.stem(peak_group['mz'], -1 * peak_group['intensity'], basefmt='grey',
                  linefmt=f'C{n + 1}-', label=isoform_id, markerfmt='None')
 
-    plt.legend()
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.title(create_modified_sequence((precursor_entry['sequence'], precursor_entry['mods'], precursor_entry['mod_sites']),
                                        nterm='', cterm=''))
     
